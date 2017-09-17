9b022a65e447002babeefa5d97b6fd933619531b
sonal-s <sonalchanchal@gmail.com>
2017-09-17 22:03:34 +0530

    Add files via upload

---
+++ 15.3.c.R
@@ -0,0 +1,22 @@
+num = as.integer(readline(prompt="Enter a number: "))
+Enter a number: 179
+flag = 0
+# prime numbers are always greater than 1
+if(num > 1) {
+  # check for factors
+  flag = 1
+  for(i in 2:(num-1)) {
+    if ((num %% i) == 0) {
+      flag = 0
+      break
+    }
+  }
+} 
+
+if(num == 2)    flag = 1
+if(flag == 1) {
+  print(paste(num,"PRIME"))
+} else {
+  print(paste(num,"NOT PRIME"))
+}
+[1] "179 PRIME"
